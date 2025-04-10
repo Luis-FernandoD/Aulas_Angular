ba60db46e6e9104b9051fa09ce7c852c51b41d32
luisf <luisfernandod.teixeira@gmail.com>
2025-04-10 16:46:10 -0300

    Aprendendo sobre input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-aulas';
+  cardPlanType = 'Simples';
+  cardPlanPrice = 100;
+  
+  
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+  @Input() planType: string = '';
+  @Input() planPrice: number = 0;
 
 }
