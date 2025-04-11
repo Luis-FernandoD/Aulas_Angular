676ae975ba842eca144e6795c7841b2888c28121
luisf <luisfernandod.teixeira@gmail.com>
2025-04-11 11:46:27 -0300

    ajustes

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -10,33 +10,15 @@ export class CardComponent {
 inputType = "text";
 isDisabled = true;
 
-private _plantype: string = '';
-@Input('planType') set planType(value: string) {
-  this._plantype = value.toUpperCase();
-}
-get planType(): String {
-  return this._plantype;
-}
+
+@Input({alias: 'planType', transform:(value:string) => value.toUpperCase()}) planType: string = ''; 
+  
 
 @Input({ required: true, alias: 'planPriceAlias' }) planPrice: number = 0;
-buttonClicked(valueEmitted: boolean) {
+  buttonClicked(valueEmitted: boolean) {
   console.log('buttonClicked', valueEmitted);
   console.log('planType', this.planType);
   console.log('planPrice', this.planPrice);
-// }
-// enableInput(){
-//   this.isDisabled = false;
-// }
-
-// disableInput(){
-//   this.isDisabled = true;
-// }
-
-// setPasswordTypeInput(){
-//   this.inputType = "password";
-// }
-
-// setTextTypeInput(){
-//   this.inputType = "text";
+  }
 }
-}
+ 
